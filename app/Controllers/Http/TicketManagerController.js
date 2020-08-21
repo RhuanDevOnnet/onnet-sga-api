df796bd7c55e261477904e94ad2bbf2b48ba7726
Eduardo <desenvolvimento03.onnetmais@gmail.com>
2020-08-21 10:10:40 -0300

    Correção de transferência de ticket

--- app/Controllers/Http/TicketManagerController.js
+++ app/Controllers/Http/TicketManagerController.js
@@ -15,7 +15,7 @@ class TicketManagerController {
           .json({ message: 'Não foi encontrado nenhum ticket com esse ID ' })
       }
 
-      if (ticket.operador_responsavel != auth.user.id) {
+      if (ticket.operador.user_id != auth.user.id) {
         return response
           .status(400)
           .json({
