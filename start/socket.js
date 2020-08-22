d4fe4ffbc2f1e18ff3e3e721c5adbb3c81bce650
Eduardo <desenvolvimento03.onnetmais@gmail.com>
2020-08-22 09:54:44 -0300

    Alterações no socket Ticket Has Finished

--- start/socket.js
+++ start/socket.js
@@ -1,5 +1,6 @@
 const server = use("Server");
 const io = use("socket.io")(server.getInstance());
+
 const { userJoin, getCurrentUser } = use(
   "./../app/Controllers/Ws/TicketSocketController"
 );
@@ -9,41 +10,35 @@ var connectedUsers = [];
 io.on("connection", function (socket) {
   connectedUsers.push(socket.id);
 
-  console.log(connectedUsers);
-
   socket.on("joinTicket", (data) => {
-    // id of room is ticket.id + assunto
 
-    console.log(data);
-    if (data == null || data == "") {
-      console.log(" sem informação do usuario");
+    // Room's id = ticket.id + assunto
+    if (!data) {
+      console.log("Sem informação do usuario");
       return;
     }
+
     let roomName = `${data.ticketId}-${data.assunto}`;
     const user = userJoin(socket.id, data.username, roomName);
-    console.log(user);
+
     socket.join(user.ticket);
   });
 
   // Listen for chatMessages
-
   socket.on("chatMessages", (msg) => {
-    console.log(msg);
-
     const user = getCurrentUser(socket.id);
 
-    const rooms = Object.keys(socket.rooms);
-    console.log(rooms);
-
-    
-    io.to(user.ticket).emit("new message", { data: "duhsaudaih" });
+    io.to(user.ticket).emit("newMessage", { data: msg });
   });
 
-  socket.on('ticketChanges', (msg) => {
-
-    console.log('teve mudança ')
-    
+  socket.on('ticketChanges', () => {
     io.emit('ticketHasChanged');
-    
   });
+
+  // Finish ticket for all the users room
+  socket.on('finishTicket', () => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.ticket).emit("ticketHasFinished", {})
+  })
 });
